09fbf4a89fdda0f6674c176f21a9a31de8cab2ce
quentinGee <quentin.geeraert1@gmail.com>
2017-05-02 22:21:35 +0200

    correct v2 localStorage tabEnvironment

--- App/www/js/settingsCtrl.js
+++ App/www/js/settingsCtrl.js
@@ -7,17 +7,35 @@ var settings = angular.module('settings', [])
 settings.controller('settingsCtrl', ['$scope',
     function($scope)
     {
-        var tabEnvironment = [localStorage.tabEnvironment];
+
+        var tabEnvironment;
+        var showEnv = document.getElementById('showEnv');
+
+        if ((typeof (localStorage.tabEnvironment)) !== "undefined"){
+            var obj = JSON.parse(localStorage.tabEnvironment);
+            tabEnvironment = obj;
+            console.log(typeof obj);
+            console.log(obj);
+            for (var i=0;i<obj.length;i++){
+                var span = document.createElement('span');
+                span.innerHTML = obj[i] + "<br/>";
+                showEnv.appendChild(span);
+            }
+        }
+        else{
+            tabEnvironment = [];
+        }
+
 
         $scope.submitFormSettings = function(){
             tabEnvironment.push($scope.environment);
             $scope.environment = "";
 
             localStorage.setItem("tabEnvironment",(JSON.stringify(tabEnvironment)));
-            var obj = JSON.parse(localStorage.tabEnvironment);
-            console.log(typeof obj);
+//            var obj = JSON.parse(localStorage.tabEnvironment);
+//            console.log(typeof obj);
 
-            console.log(obj[2]);
+//            console.log(obj[2]);
         }
     }
 ]);
