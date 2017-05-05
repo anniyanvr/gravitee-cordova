8b5dbe4f54ef24bedbded1edaa7d7489f744d045
quentinGee <quentin.geeraert1@gmail.com>
2017-05-05 17:22:15 +0200

    add replace and delete environment

--- App/platforms/android/assets/www/js/settingsCtrl.js
+++ App/platforms/android/assets/www/js/settingsCtrl.js
@@ -11,21 +11,37 @@ settings.controller('settingsCtrl', ['$scope',
         var tabEnvironment;
         var showEnv = document.getElementById('showEnv');
 
+        deleteEnv = function (i) {
+            var obj = JSON.parse(localStorage.tabEnvironment);
+            console.log(i);
+            obj.splice(i,1);
+            tabEnvironment = obj;
+            localStorage.setItem("tabEnvironment",(JSON.stringify(tabEnvironment)));
+            location.reload();
+        }
+
         if ((typeof (localStorage.tabEnvironment)) !== "undefined"){
             var obj = JSON.parse(localStorage.tabEnvironment);
+            //obj.splice(0,1)
             tabEnvironment = obj;
+            //localStorage.setItem("tabEnvironment",(JSON.stringify(tabEnvironment)));
             console.log(typeof obj);
             console.log(obj);
             for (var i=0;i<obj.length;i++){
                 var span = document.createElement('span');
 
+                console.log(i);
+
                 span.innerHTML =
                     "<table class='striped'>" +
-                    "   <tr>" +
+                    "   <tr class='number'>" +
                     "       <td style='padding-left: 15px; font-size: small; width: 100%'>" + obj[i] + "<td>" +
                     "       <!-- <td style='cursor: pointer; padding-right: 15px; text-align: right; width: 20%'>" +
-                    "           <i class='material-icons'>delete</i>" +
+                    "           <i class='material-icons'>edit</i>" +
                     "       </td> -->" +
+                    "       <td onclick='deleteEnv("+i+")' style='cursor: pointer; padding-right: 15px; text-align: right; width: 20%'>" +
+                    "           <i class='material-icons'>delete</i>" +
+                    "       </td>" +
                     "   </tr>" +
                     "</table>";
                 showEnv.appendChild(span);
--- App/www/js/settingsCtrl.js
+++ App/www/js/settingsCtrl.js
@@ -11,21 +11,60 @@ settings.controller('settingsCtrl', ['$scope',
         var tabEnvironment;
         var showEnv = document.getElementById('showEnv');
 
+        // delete Environment
+        deleteEnv = function (i) {
+            var obj = JSON.parse(localStorage.tabEnvironment);
+            console.log(i);
+            obj.splice(i,1);
+            tabEnvironment = obj;
+            localStorage.setItem("tabEnvironment",(JSON.stringify(tabEnvironment)));
+            location.reload();
+        }
+
+        // edit Environment
+        var tempo;
+        editEnv = function (i) {
+            tempo = 0;
+            tempo = i;
+            var obj = JSON.parse(localStorage.tabEnvironment);
+            $scope.environmentEditInput = obj[i];
+            document.getElementById('modalEdit').setAttribute('style','top:60px; bottom:50px; display:inline-block; z-index:1');
+        }
+
+        $scope.submitFormEdit = function () {
+            console.log(tempo);
+            var obj = JSON.parse(localStorage.tabEnvironment);
+            obj.splice(tempo,1,$scope.environmentEdit);
+            console.log(obj);
+            tabEnvironment = obj;
+            localStorage.setItem("tabEnvironment",(JSON.stringify(tabEnvironment)));
+            location.reload();
+            $scope.environmentEdit ="";
+        }
+
         if ((typeof (localStorage.tabEnvironment)) !== "undefined"){
             var obj = JSON.parse(localStorage.tabEnvironment);
+            //obj.splice(0,1)
             tabEnvironment = obj;
+            //localStorage.setItem("tabEnvironment",(JSON.stringify(tabEnvironment)));
             console.log(typeof obj);
             console.log(obj);
             for (var i=0;i<obj.length;i++){
                 var span = document.createElement('span');
 
+                console.log(i);
+
                 span.innerHTML =
                     "<table class='striped'>" +
                     "   <tr>" +
-                    "       <td style='padding-left: 15px; font-size: small; width: 100%'>" + obj[i] + "<td>" +
-                    "       <!-- <td style='cursor: pointer; padding-right: 15px; text-align: right; width: 20%'>" +
+                    "       <td id='input"+i+"' style='padding-left: 15px; font-size: small; width: 100%'> " +
+                    "           <input style=' color: #000;' value='"+obj[i]+"' disabled><td>" +
+                    "       <td onclick='editEnv("+i+")' style='cursor: pointer; padding-right: 15px; text-align: right; width: 20%'>" +
+                    "           <i class='material-icons'>edit</i>" +
+                    "       </td>" +
+                    "       <td onclick='deleteEnv("+i+")' style='cursor: pointer; padding-right: 15px; text-align: right; width: 20%'>" +
                     "           <i class='material-icons'>delete</i>" +
-                    "       </td> -->" +
+                    "       </td>" +
                     "   </tr>" +
                     "</table>";
                 showEnv.appendChild(span);
