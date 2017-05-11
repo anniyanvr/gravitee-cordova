b591efe8edef474c02412c5e1cbed7901ed764ec
quentinGee <quentin.geeraert1@gmail.com>
2017-05-11 10:21:05 +0200

    health-check v0.1

--- App/www/js/controller/apis/apiCtrlHealthCheck.js
+++ App/www/js/controller/apis/apiCtrlHealthCheck.js
@@ -7,7 +7,6 @@ function apisCtrlHealthCheck($scope, $routeParams, $http) {
     var constant = localStorage.baseURL+"constants.json";
 
     var selectElmt = document.getElementById('select_time');
-    var value = selectElmt.options[selectElmt.selectedIndex].value;
     var text = selectElmt.options[selectElmt.selectedIndex].text;
 
     // timestamp date
@@ -18,13 +17,12 @@ function apisCtrlHealthCheck($scope, $routeParams, $http) {
     var hour    = ('0'+now.getHours()  ).slice(-2);
     var min     = ('0'+now.getMinutes()).slice(-2);
     var second  = ('0'+now.getSeconds()).slice(-2);
-    var date_1  = ( month +'/'+'0'+(day-1)+'/'+year+' '+hour+':'+min+':'+second);
+
+    // create date time - default
+    var lastHour = ( month +'/'+day+'/'+year+' '+'0'+(hour-1)+':'+min+':'+second);
     var date  = ( month +'/'+day+'/'+year+' '+hour+':'+min+':'+second);
 
-    // set value - dafault
-    $scope.date_1 = date_1;
-    $scope.date = date;
-    $scope.timestamp_interval = value;
+    // set value - default
     $scope.Time_type = text;
 
     httpSuccessAPIHealthCheck = function (response) { $scope.rep = response; }
@@ -36,6 +34,12 @@ function apisCtrlHealthCheck($scope, $routeParams, $http) {
     }
 
     function healtcheckFunction(val,d_1,d) {
+        // set value
+        $scope.date_1 = d_1;
+        $scope.date = date;
+        $scope.timestamp_interval = val;
+
+        // default - lastHour
         var healthcheck = localStorage.baseURL + "management/apis/" + id +"/health?interval="+val+"&from="+toTimestamp(d_1)+"344&to="+toTimestamp(d)+"344";
         $http.get(healthcheck, {
             headers: {'Authorization': 'Basic ' + encode(localStorage.username + ':' + localStorage.password)}
@@ -54,25 +58,28 @@ function apisCtrlHealthCheck($scope, $routeParams, $http) {
             document.location.href = "index.html";
         });
 
-        healtcheckFunction(value,date_1,date);
+        healtcheckFunction("60000",lastHour,date);
     });
 
     $scope.changeTime = function () {
         var selectElmt = document.getElementById('select_time');
-        var value = selectElmt.options[selectElmt.selectedIndex].value;
-        var text = selectElmt.options[selectElmt.selectedIndex].text;
-        var now = new Date();
-        var year    = now.getFullYear();
-        var month   = ('0'+(now.getMonth()+1));
-        var day     = ('0'+now.getDate()   ).slice(-2);
-        var hour    = ('0'+now.getHours()  ).slice(-2);
-        var min     = ('0'+now.getMinutes()).slice(-2);
-        var second  = ('0'+now.getSeconds()).slice(-2);
-        var date_1  = ( month +'/'+'0'+(day-1)+'/'+year+' '+hour+':'+min+':'+second);
+        var text = selectElmt.options[selectElmt.selectedIndex].value;
+        $scope.Time_type = text;
+
+        var last5min = ( month +'/'+day+'/'+year+' '+hour+':'+(min-5)+':'+second);
+        var lastHour = ( month +'/'+day+'/'+year+' '+'0'+(hour-1)+':'+min+':'+second);
+        var last24Hours  = ( month +'/'+(day-1)+'/'+year+' '+hour+':'+min+':'+second);
+        var last3Days  = ( month +'/'+'0'+(day-3)+'/'+year+' '+hour+':'+min+':'+second);
+        var lastMonth  = ( '0'+(month-1) +'/'+day+'/'+year+' '+hour+':'+min+':'+second);
+        var last3Months  = ( '0'+(month-3) +'/'+day+'/'+year+' '+hour+':'+min+':'+second);
         var date  = ( month +'/'+day+'/'+year+' '+hour+':'+min+':'+second);
-        $scope.date_1 = date_1; $scope.date = date;
-        $scope.timestamp_interval = value; $scope.Time_type = text;
-        healtcheckFunction(value,date_1,date);
+
+        if (text === "last5minutes"){ healtcheckFunction("10000"        ,last5min       ,date); }
+        if (text === "lastHour")    { healtcheckFunction("60000"        ,lastHour       ,date); }
+        if (text === "last24Hours") { healtcheckFunction("3600000"      ,last24Hours    ,date); }
+        if (text === "last3Days")   { healtcheckFunction("10800000"     ,last3Days      ,date); }
+        if (text === "lastMonth")   { healtcheckFunction("10000000"     ,lastMonth      ,date); }
+        if (text === "last3Months") { healtcheckFunction("10000000"     ,last3Months    ,date); }
     }
 
 
