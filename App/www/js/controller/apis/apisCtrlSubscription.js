ca866f392c5f862645ed68004976cff5446de4b3
quentinGee <quentin.geeraert1@gmail.com>
2017-06-15 14:26:12 +0200

    add key - subscriptions

--- App/www/js/controller/apis/apisCtrlSubscription.js
+++ App/www/js/controller/apis/apisCtrlSubscription.js
@@ -7,10 +7,24 @@ function apisCtrlSubscription($scope, $routeParams, $http) {
 
     httpSuccessAPI = function (response) { $scope.rep = response; $scope.statusShow = "accepted";}
     httpSuccessAPISubscription = function (response) { $scope.subscription = response; }
-
+    httpSuccessAPIKeys = function ( response ) {
+        Materialize.toast( response[0].key, 6000, 'rounded') // 'rounded' is the class I'm applying to the toast
+    }
+    
     var api = localStorage.baseURL+"management/apis/"+id+"/"; // with login
     var apiSub = localStorage.baseURL+"management/apis/"+id+"/subscriptions"; // with login
-
+    
+    $scope.ShowKey = function (sub_id) {
+        var apiSubKey = localStorage.baseURL+"management/apis/"+id+"/subscriptions/"+sub_id+"/keys";
+        $http.get(apiSubKey,{
+            headers: {
+                'Authorization': 'Basic ' + localStorage.authorization
+            }
+        }).success(httpSuccessAPIKeys).error(function () {
+            document.location.href="index.html";
+        }); 
+    }
+    
     $http.get(api,{
         headers: {
             'Authorization': 'Basic ' + localStorage.authorization
